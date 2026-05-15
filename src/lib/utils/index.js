64a4d2d1b13ef0b1b5a3ea2c3d205140a411dea3
Jie-Ting Jiang <jieting.jiang@isi-hagenberg.at>
2026-05-15 12:27:31 +0800

    feat: add missing barrel exports for tweenCamera and buildPointOctree

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -4,3 +4,5 @@ export { SelectiveBloom } from './SelectiveBloom';
 export { generateData } from './generateData';
 export { palette } from './color';
 export { DATA_SOURCES, DEFAULT_DATA_SOURCE_ID } from './dataSources';
+export { tweenCamera, setCameraPosition, setControlsTarget, tweenGroup } from './tweenCamera';
+export { buildPointOctree } from './buildPointOctree';
